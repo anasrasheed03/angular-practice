8ac54e9f39a99efec17de096558d91954f810be1
anasrasheed03 <anas.rasheed@systemsltd.com>
2020-06-27 21:53:19 +0500

    assignment 2: interpolation, event binding, property binding, component lifecycle

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  public title:string;
+  public name:string;
+  public email:string;
+  public isChecked:boolean;
+  public password:string;
   constructor() { }
 
   ngOnInit() {
+    this.email = 'abc@emcil.com'
+    this.title = 'Angular Practice'
   }
 
+  public printName(){
+    return this.name = 'Anas ur Rasheed'
+  }
+
+  public onCheckBoxClick(event:any){
+    this.isChecked=event.target.checked
+  }
+
+  public onSubmit(){
+    console.log(`Email: ${this.email} and Password: ${this.password ? this.password : 'no password'}`)
+  }
+
+  public getEmail(event:any){
+    console.log(`Entered Email: ${event.target.value}`)
+  }
 }
